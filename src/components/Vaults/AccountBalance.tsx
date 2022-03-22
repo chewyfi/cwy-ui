47bbfe3aa5797e812d4aeb566fc2b1fe89a3c225
VarunVerma7 <varunverma855@gmail.com>
2022-03-22 15:33:44 -0400

    More refactor

--- src/components/Vaults/AccountBalance.tsx
+++ src/components/Vaults/AccountBalance.tsx
@@ -10,7 +10,38 @@ import {
   WETH_TOKEN_CONTRACT
 } from '../../utils/constants'
 
-export const AccountBalance = (specificAccount) => {
+interface Props {
+  balance: string | number
+  name: string
+  suffix: string
+}
+
+export const AccountBalance = (name) => {
+  console.log('NAME ', JSON.stringify(name))
+
+  const getBalance = (token: string) => {
+    switch (token) {
+      case 'MOVR':
+        return movr?.formatted
+      case 'WETH':
+        return weth?.formatted
+      case 'WBTC':
+        return wbtc?.formatted
+      case 'USDC':
+        return usdc?.formatted
+      case 'FRAX':
+        return frax?.formatted
+      case 'USDT':
+        return usdt?.formatted
+      case 'solar3POOL':
+        return threePool?.formatted
+      case 'solar3FRAX':
+        return frax3Pool?.formatted
+      case 'solarstKSM':
+        return solarstKSM?.formatted
+    }
+  }
+
   const [{ data: account }] = useAccount()
   const [{ data: movr }] = useBalance({
     addressOrName: account?.address
@@ -48,5 +79,45 @@ export const AccountBalance = (specificAccount) => {
     addressOrName: account?.address
   })
 
-  return
+  return (
+    <div className="flex space-x-2">
+      <div className="mt-1 mb-3 text-gray-500">
+        <label className="mb-1 text-[11px]">
+          Balance: {getBalance(name['name'])} LP
+        </label>
+        <div className="flex items-center text-[12px]">
+          <input
+            type="number"
+            placeholder="0.0"
+            className="w-full px-2 py-1 border-2 border-r-0 border-gray-200 rounded-l-lg outline-none"
+          />
+          <button className="px-2 py-1 font-semibold bg-white border-2 border-l-0 border-gray-200 rounded-r-lg focus:outline-none">
+            max
+          </button>
+        </div>
+        <button
+          // onClick={() => approve(item.contracts, provider)}
+          className="inline-block w-full p-1 mt-1 text-white bg-black border-2 border-black rounded-lg"
+        >
+          Approve
+        </button>
+      </div>
+      <div className="mt-1 mb-3 text-gray-500">
+        <label className="mb-1 text-[11px]">Deposited: 0</label>
+        <div className="flex items-center text-[12px]">
+          <input
+            type="number"
+            placeholder="0.0"
+            className="w-full px-2 py-1 border-2 border-r-0 border-gray-200 rounded-l-lg outline-none"
+          />
+          <button className="px-2 py-1 font-semibold bg-white border-2 border-l-0 border-gray-200 rounded-r-lg focus:outline-none">
+            max
+          </button>
+        </div>
+        <button className="inline-block w-full p-1 mt-1 text-gray-500 border-2 border-gray-300 rounded-lg">
+          Withdraw
+        </button>
+      </div>
+    </div>
+  )
 }
--- src/components/Vaults/Vault.tsx
+++ src/components/Vaults/Vault.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { useState } from 'react'
 
 import { poolAddresses } from '../../chain-info/pool-addresses'
+import { AccountBalance } from './AccountBalance'
 
 const APYS = [
   {
@@ -155,52 +156,12 @@ export const Vault: React.FC<Props> = ({ item, index, balance }) => {
         </span> */}
         </div>
       </Disclosure.Button>
-      {item.isOpen && (
+      {true && (
         <Disclosure.Panel
           static
           className="px-2 text-sm bg-gray-100 rounded-b-lg"
         >
-          <div className="flex space-x-2">
-            <div className="mt-1 mb-3 text-gray-500">
-              <label className="mb-1 text-[11px]">
-                Balance: {balance} {item.suffix}
-              </label>
-              <div className="flex items-center text-[12px]">
-                <input
-                  type="number"
-                  placeholder="0.0"
-                  className="w-full px-2 py-1 border-2 border-r-0 border-gray-200 rounded-l-lg outline-none"
-                />
-                <button className="px-2 py-1 font-semibold bg-white border-2 border-l-0 border-gray-200 rounded-r-lg focus:outline-none">
-                  max
-                </button>
-              </div>
-              <button
-                // onClick={() => approve(item.contracts, provider)}
-                className="inline-block w-full p-1 mt-1 text-white bg-black border-2 border-black rounded-lg"
-              >
-                Approve
-              </button>
-            </div>
-            <div className="mt-1 mb-3 text-gray-500">
-              <label className="mb-1 text-[11px]">
-                Deposited: 0 {item.name} {item.suffix}
-              </label>
-              <div className="flex items-center text-[12px]">
-                <input
-                  type="number"
-                  placeholder="0.0"
-                  className="w-full px-2 py-1 border-2 border-r-0 border-gray-200 rounded-l-lg outline-none"
-                />
-                <button className="px-2 py-1 font-semibold bg-white border-2 border-l-0 border-gray-200 rounded-r-lg focus:outline-none">
-                  max
-                </button>
-              </div>
-              <button className="inline-block w-full p-1 mt-1 text-gray-500 border-2 border-gray-300 rounded-lg">
-                Withdraw
-              </button>
-            </div>
-          </div>
+          <AccountBalance name={item.name} />
         </Disclosure.Panel>
       )}
     </Disclosure>

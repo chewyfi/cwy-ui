6acbf2b9ba705edf3c9820708194a4ac27643d67
VarunVerma7 <varunverma855@gmail.com>
2022-03-22 12:27:10 -0400

    Added contract addresses

---
+++ src/chain-info/pool-addresses.js
@@ -0,0 +1,52 @@
+export const contracts = {
+    Treasure: '0x9E7e642DEA4a7C95F70CA3B6f1d15A416097517A',
+    MoonbeamUSDC: {
+      Vault: '0x934c5419286B2F5722871c1206E4B8E9c59246E0',
+      Strategy: '0xaa0df6a1dbe11848fa4f824abd076d994166b94e',
+      Want: '0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d'
+    },
+    MoonbeamFRAX: {
+      Vault: '0xb3f9f791ad5aa16e92a58d86f4e8ed9cd985dbe0',
+      Strategy: '0x5be32da7627570c34da15b3410a15d6723d58306',
+      Want: '0x1a93b23281cc1cde4c4741353f3064709a16197d'
+    },
+    MoonbeamUSDT: {
+      Vault: '0x3A13fF87166B85ccfa0f360A238C6dd996b6aD39',
+      Strategy: '0xB1793CF2806Fd2F9a1096eb12e7f9dd975462372',
+      Want: '0xB44a9B6905aF7c801311e8F4E76932ee959c663C'
+    },
+    MoonbeamETH: {
+      Vault: '0x25F891fC8d9D63D68DB3815EFAEE6225a3421F6a',
+      Strategy: '0xF62D54f768A3bbE1ca2008125B35E79C38a279f2',
+      Want: '0x639A647fbe20b6c8ac19E48E2de44ea792c62c5C'
+    },
+    MoonbeamMOVR: {
+      Vault: '0x118CE6bCDb827a14C12dF2DeDd6c5af02904B895',
+      Strategy: '0x9CA8902E42300d5E2Afb890F690bC1139c965A23',
+      Want: '0x98878B06940aE243284CA214f92Bb71a2b032B8A '
+    },
+    Solarbeam3pool: {
+      Vault: '0x76fD71dDC47bfC2E8376767900bb527730915972',
+      Strategy: '0xF9BA7682f0A059077AAa194f55fce9E76d252fd9',
+      Want: '0xfb29918d393AaAa7dD118B51A8b7fCf862F5f336'
+    },
+    MoonbeamBTCSupplyOnly: {
+      Vault: '0xaEE11188f88AF3Dc72C7E68BDC3dF1E2B5665556',
+      Strategy: '0x56dc314E5Be27cA60Cb60C725fc1585F0e20Dcc9',
+      Want: '0x6aB6d61428fde76768D7b45D8BFeec19c6eF91A8'
+    },
+    SolarbeamFrax3pool: {
+      Vault: '0x523Da6222Bd5127afD3E4f3d180ca521D5aC964D',
+      Strategy: '0xaeC10e47362B3e4513432aF28d574460A0027CE8',
+      Want: '0x884609A4D86BBA8477112E36e27f7A4ACecB3575',
+      PoolId: '9'
+    },
+    SolarbeamstKSMpool: {
+      Vault: '0x06B88540881779FA0e930F3F92d52a9Df3f4E14D',
+      Strategy: '0x7DAEDEF2e6C3644b166d4dC28A77A5609d1160Cb',
+      Want: '0x493147C85Fe43F7B056087a6023dF32980Bcb2D1',
+      PoolId: 13
+    }
+  }
+  
+  

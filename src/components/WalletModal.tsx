24411c23c127d24ea5eb8f7cb4881d2e3a906848
Sasi <me@sasi.codes>
2022-03-27 13:56:11 +0530

    wallet modal overlay

--- src/components/WalletModal.tsx
+++ src/components/WalletModal.tsx
@@ -1,3 +1,4 @@
+import { Dialog } from '@headlessui/react'
 import clsx from 'clsx'
 import React from 'react'
 
@@ -12,15 +13,18 @@ interface Props {
 
 const WalletModal: React.FC<Props> = (props) => {
   return (
-    <div
+    <Dialog
+      onClose={props.onClose}
+      as="div"
+      open={props.show}
       className={clsx(
         'top-0 bottom-0 z-20 left-0 right-0 min-h-screen bg-gray-100 bg-opacity-70 backdrop-filter backdrop-blur flex flex-row items-start justify-center',
         {
-          hidden: !props.show,
           absolute: props.show
         }
       )}
     >
+      <Dialog.Overlay className="fixed inset-0" />
       <div className="relative z-20 max-w-lg w-full md:w-1/2 shadow flex flex-col mt-20 rounded-2xl items-center mx-4 bg-white lg:mt-[10%] lg:mx-0 min-h-30">
         <div className="flex items-center justify-between w-full p-3 px-4 pb-1">
           <h1 className="opacity-80">Choose your wallet</h1>
@@ -33,7 +37,7 @@ const WalletModal: React.FC<Props> = (props) => {
         </div>
         <WalletSelector onClose={props.onClose} />
       </div>
-    </div>
+    </Dialog>
   )
 }
 

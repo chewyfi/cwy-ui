afc85469a134abe431ba15c382bdaeddcd102e6b
Sasi <me@sasi.codes>
2022-03-27 12:03:37 +0530

    leading table header

--- src/components/Vaults/TableHeader.tsx
+++ src/components/Vaults/TableHeader.tsx
@@ -4,8 +4,8 @@ export const TableHeader = () => {
       <div className="text-[12px] flex py-3 font-medium items-center w-full">
         <span className="flex items-center w-2/4">
           <span className="flex flex-col items-start w-1/4 ml-3">
-            <span>Token</span>
-            <span>Strategy</span>
+            <span className="-mb-1">Token</span>
+            <span className="-mb-1">Strategy</span>
             <span className="flex items-center space-x-2">
               TVL
               <img className="h-2 ml-3" src="/static/down-arrow.svg" alt="" />
@@ -18,7 +18,7 @@ export const TableHeader = () => {
         </span>
         <span className="flex justify-end w-1/4 mr-6 ">
           <div className="flex flex-col text-left">
-            <span>Balance</span>
+            <span className="-mb-1">Balance</span>
             <span>Deposited</span>
           </div>
         </span>

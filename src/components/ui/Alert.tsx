64afaf586d53abcc4096a8952dd463b34185a43d
Sasi <me@sasi.codes>
2022-03-30 19:38:52 +0530

    design cleanups

--- src/components/ui/Alert.tsx
+++ src/components/ui/Alert.tsx
@@ -8,7 +8,7 @@ export const Alert: React.FC<AlertProps> = ({ errorMessage }) => {
       className="relative px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded"
       role="alert"
     >
-      <strong className="font-bold">Error</strong>
+      <strong className="mr-2 font-bold">Error</strong>
       <span className="block sm:inline">{errorMessage}.</span>
       <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
         <svg
--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,16 +1,22 @@
+import clsx from 'clsx'
+
 interface Props {
   size?: 'xs' | 'sm' | 'md'
+  className?: string
 }
 
-export const Spinner: React.FC<Props> = ({ size = 'sm' }) => {
+export const Spinner: React.FC<Props> = ({ size = 'sm', className }) => {
   return (
     <div
-      className={`
-      rounded-full animate-spin border-gray-300 border-t-gray-500
-      ${size === 'xs' && 'h-3 w-3 border-[2px]'}
-      ${size === 'sm' && 'h-5 w-5 border-2'}
-      ${size === 'md' && 'h-5 w-5 border-[3px]'}
-      `}
+      className={clsx(
+        'rounded-full animate-spin border-gray-300 border-t-gray-500',
+        {
+          'h-3 w-3 border-[2px]': size === 'xs',
+          'h-5 w-5 border-2': size === 'sm',
+          'h-5 w-5 border-[3px]': size === 'md'
+        },
+        className
+      )}
     />
   )
 }

f793fd0e98fdb77b3b4e8ce808a239634ed8a175
Sasi <me@sasi.codes>
2022-05-04 20:31:15 +0530

    feat: landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,68 @@
+import Head from 'next/head'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import Layout from 'src/components/shared/Layout'
+
+const footerItems = [
+  {
+    href: 'https://twitter.com/chewyfi',
+    logo: '/static/landing/twitter.svg',
+    className: 'w-8 h-8'
+  },
+  {
+    href: 'http://discord.gg/m7TWUjBc9v',
+    logo: '/static/landing/discord.svg',
+    className: 'w-8 h-8'
+  },
+  {
+    href: 'https://t.me/chewyfi',
+    logo: '/static/landing/telegram.svg',
+    className: 'w-8 h-8'
+  },
+  {
+    href: 'https://chewyfi.substack.com',
+    logo: '/static/landing/substack.svg',
+    className: 'w-7 h-7'
+  },
+  {
+    href: 'https://github.com/chewyfi',
+    logo: '/static/landing/github.svg',
+    className: 'w-8 h-8'
+  }
+]
 
 export default function Vaults() {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.push('vaults')
-  }, [])
-
   return (
-    <Layout headerTitle="Home" hideFooter>
-      <div className="grid h-full mt-8 place-items-center">
-        <div className="text-center">
-          <div className="mb-5 font-bold">Coming soon</div>
-          <Link href="/vaults">
-            <a className="font-semibold underline">Explore Vaults</a>
+    <div className="grid h-screen place-items-center bg-gradient-to-b to-[#ff6486] from-[#ffafbe]">
+      <Head>
+        <title>Chewy</title>
+      </Head>
+      <div className="font-bold text-center text-white">
+        <h1 className="text-[51px]">Chewy</h1>
+        <p className="text-[20px]">
+          Foundational layer for interest-bearing tokens.
+        </p>
+        <div className="my-10">
+          <Link href="https://app.sushi.com/miso">
+            <a target="_blank" rel="noreferrer">
+              <button className="transition ease-in-out delay-75 hover:scale-105 duration-300 text-[20px] uppercase text-[#ff6486] py-3 px-6 bg-white rounded-lg shadow">
+                Enter the presale
+              </button>
+            </a>
           </Link>
         </div>
+        <div className="flex justify-center space-x-5">
+          {footerItems.map((f, idx) => (
+            <a
+              className="flex items-center"
+              target="_blank"
+              href={f.href}
+              key={idx}
+              rel="noreferrer"
+            >
+              <img src={f.logo} alt="" className={f.className} />
+            </a>
+          ))}
+        </div>
       </div>
-    </Layout>
+    </div>
   )
 }
